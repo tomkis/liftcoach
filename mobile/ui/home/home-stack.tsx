3e2113c3d63f7266071bfc64e6842e38fe02e125
Tomas Weiss <tomas.weiss2@gmail.com>
2026-02-13 13:01:31 +0100

    chore: removed home testing from the stack

--- mobile/ui/home/home-stack.tsx
+++ mobile/ui/home/home-stack.tsx
@@ -1,12 +1,11 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { HomeTestingStack } from '@/mobile/ui/home-testing/home-testing-stack'
+import { trpc } from '@/mobile/trpc'
+import { EmptyWrapper } from '@/mobile/ui/components/empty-wrapper'
 import { MicrocycleProgressView } from '@/mobile/ui/home/microcycle-progress/microcycle-progress-view'
 import { PlanningStack } from '@/mobile/ui/mesocycle-planner/planning-stack'
 import { WorkoutStack } from '@/mobile/ui/workout/workout-stack'
-import { trpc } from '@/mobile/trpc'
-import { EmptyWrapper } from '@/mobile/ui/components/empty-wrapper'
 
 import { HomeTabBar } from './home-tab-bar'
 
@@ -51,7 +50,6 @@ export const HomeStack = () => {
   return (
     <Stack.Navigator initialRouteName="TabNavigator" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="TabNavigator" component={TabNavigator} />
-      <Stack.Screen name="HomeTesting" component={HomeTestingStack} />
     </Stack.Navigator>
   )
 }
